55dc720c2f126788a773b49a4d02da531203b07b
Kai Hampton <kaili.hampton@colostate.edu>
2026-09-03 12:18:53 -0600

    add skills for later api

--- server/server.js
+++ server/server.js
@@ -1,20 +1,15 @@
 // Import the Express module for creating the server
 const express = require("express");
-
 // Import the CORS module to handle cross-origin requests
 const cors = require("cors");
-
 // Create an instance of the Express application
 const app = express();
-
 // Define CORS options to allow requests from the specified origin
 const corsOpts = {
     origin: "http://localhost:5173",
 }
-
 // Use the CORS middleware with the defined options
 app.use(cors(corsOpts));
-
 // Define route for the API endpoint
 app.get("/api", (req, res) => {
     res.json({ fruits: ["apple", "orange", "banana"] });
